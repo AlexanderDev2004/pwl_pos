9eb45011533c84af62709f8880b577604a87915d
AlexanderDev2004 <alexgaming2120@gmail.com>
2025-03-04 21:12:31 +0700

    feat(crud) : Praktikum 2.6 – Create, Read, Update, Delete (CRUD)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -70,20 +70,20 @@ class UserController extends Controller
         //     ['nama' => 'manager']
         // );
 
-        $user = UserModel::create([
-            'username' => 'manager13',
-            'nama' => 'manager13',
-            'password' => Hash::make('12345'),
-            'level_id' => 2
-        ]);
-        $user->username = 'manager14';
-        $user->save();
-        $user-> wasChanged();
-        $user-> wasChanged('username');
-        $user-> wasChanged('username','level_id');
-        $user-> wasChanged('nama');
-        $user-> wasChanged('nama', 'username');
-        dd($user-> wasChanged(['nama', 'username']));
+        // $user = UserModel::create([
+        //     'username' => 'manager13',
+        //     'nama' => 'manager13',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 2
+        // ]);
+        // $user->username = 'manager14';
+        // $user->save();
+        // $user-> wasChanged();
+        // $user-> wasChanged('username');
+        // $user-> wasChanged('username','level_id');
+        // $user-> wasChanged('nama');
+        // $user-> wasChanged('nama', 'username');
+        // dd($user-> wasChanged(['nama', 'username']));
         // $user->isDirty();
         // $user->isDirty('username');
         // $user->isDirty('nama');
@@ -98,6 +98,89 @@ class UserController extends Controller
         // $user->isDirty();
         // $user->isclean();
         // dd($user -> isDirty());
-        return view('user', ['data' => UserModel::all()]);
+
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
     }
+    public function tambah()
+    {
+        return view('user_tambah');
+    }
+    public function tambah_simpan(Request $request)
+    {
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => Hash::make($request->password),
+            'level_id' => $request->level_id
+        ]);
+
+        return redirect('/user');
+    }
+    public function ubah_simpan($id, Request $request)
+    {
+        $user = UserModel::find($id);
+
+        // $user = UserModel::firstOrNew(
+        //     [
+        //         'username' => 'manager33',
+        //         'nama' => 'Manager Tiga Tiga',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2
+        //     ]
+        // );
+        // $user->save();
+
+        // return view('user', ['data' => $user]);
+        $user->username = $request->username;
+        $user->nama = $request->nama;
+        $user->password = Hash::make($request->password);
+        $user->level_id = $request->level_id;
+
+        $user->save();
+
+        // $user = UserModel::create([
+        //     'username' => 'manager55',
+        //     'nama' => 'Manager55',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 2,
+        // ]);
+
+        // $user->username = 'manager56';
+
+        // $user->isDirty(); // true
+        // $user->isDirty('username'); // true
+        // $user->isDirty('nama'); // false
+
+        // $user->isDirty(['nama', 'username']); // true
+
+        // $user->isClean(); // false
+        // $user->isClean('username'); // false
+        // $user->isClean('nama'); // true
+        // $user->isClean(['nama', 'username']); // false
+
+        // $user->save();
+
+        // $user->isDirty(); // false
+        // $user->isClean(); // true
+
+        // dd($user->isDirty());
+        return redirect('/user');
+    }
+
+
+    public function hapus ($id){
+        $user = UserModel::find($id);
+        $user->delete();
+
+        return redirect('/user');
+    }
+    public function ubah($id)
+    {
+        $user = UserModel::find($id);
+        return view('user_ubah', ['data' => $user]);
+    }
+
+
+
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -9,20 +9,23 @@
     <h1>Data User</h1>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
+            <td>ID</td>
             <td>Username</td>
             <td>Nama</td>
-            <td>Password</td>
-            <td>Level ID</td>
-            <td>ID</td>
+            <td>ID Level Pengguna</td>
+            <td>Aksi</td>
             {{-- <td>Jumlah pengguna</td> --}}
         </tr>
-        @foreach($data as $d)
+        @foreach ($data as $d)
             <tr>
                 <td>{{ $d->user_id }}</td>
                 <td>{{ $d->username }}</td>
                 <td>{{ $d->nama }}</td>
-                <td>{{ $d->password }}</td>
                 <td>{{ $d->level_id }}</td>
+                <td>
+                    <a href="./user/ubah/{{ $d->user_id }}">Ubah</a>
+                    <a href="./user/hapus/{{ $d->user_id }}">Hapus</a>
+                </td>
                 {{-- <td>{{ $userCount }}</td> --}}
             </tr>
         @endforeach
